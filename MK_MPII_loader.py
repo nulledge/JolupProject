f98b69137994538929362a5001844163891c34d3
tata42 <fldjzksms@gmail.com>
2018-06-03 22:40:59 +0900

    loader wip

--- MK_MPII_loader.py
+++ MK_MPII_loader.py
@@ -4,25 +4,30 @@ import os
 import skimage
 import skimage.io
 import numpy as np
+import random
 from enum_JOINT import JOINT
 from tqdm import tqdm
-from util import crop_image
+from vectormath import Vector2
+from util import crop_image, generate_heatmap
 
 class MPII:
     def __init__(self, batch_size, task='train'):
         self.mat = scipy.io.loadmat('data/mpii_human_pose_v1_u12_1.mat', squeeze_me = True, struct_as_record=False)
-        self.total_num = len(getattr(self.mat['RELEASE'], 'img_train'))
+        self.image_num = len(getattr(self.mat['RELEASE'], 'img_train'))
         self.image_path = './data/images/'
         self.joint_num = len(JOINT)
         self.cursor = 0
         self.image_set = []
         self.batch_size = batch_size
 
-        for img_idx in range(self.total_num):
-            if task == 'train' and getattr(self.mat['RELEASE'], 'img_train') == 1:
-                self.image_set.append(img_idx)
-            if task == 'test' and getattr(self.mat['RELEASE'], 'img_train') == 0:
-                self.image_set.append(img_idx)
+        for img_idx in range(self.image_num):
+            if task == 'train' and getattr(self.mat['RELEASE'], 'img_train')[img_idx] == 1:
+                annorect = getattr(getattr(self.mat['RELEASE'], 'annolist'), 'annorect')
+                if type(annorect) is np.ndarray:
+                    for r_idx in range(len(annorect)):
+                        self.image_set.append((img_idx, r_idx))
+                else:
+                    self.image_set.append(img_idx, -1)
 
     def __iter__(self):
         return self
@@ -34,6 +39,10 @@ class MPII:
     def batch_image_set(self):
         prev_cursor = self.cursor
         self.cursor += self.batch_size
+        if self.cursor > len(self.image_set):
+            self.image_set = random.shuffle(self.image_set)
+            prev_cursor = 0
+            self.cursor = self.batch_size
         return self.image_set[prev_cursor:self.cursor]
 
     def get_minibatch(self, batch_image):
@@ -47,21 +56,46 @@ class MPII:
 
         return batch_rgb, batch_heatmap, batch_keypoint, batch_activity
 
-    def get_data(self, img_idx):
+    def get_data(self, idx):
+        img_idx, r_idx = idx
         annolist = getattr(self.mat['RELEASE'], 'annolist')[img_idx]
+        if r_idx != -1:
+            annorect = getattr(annolist, 'annorect')[r_idx]
+        else:
+            annorect = getattr(annolist, 'annorect')
+
         image_name = self.image_path + getattr(getattr(annolist, 'image'), 'name')   
-        act = getattr(self.mat['RELEASE'], 'act')[img_idx]
-        
         image = skimage.img_as_float(skimage.io.imread(image_name))
-        objpos = getattr(getattr(annolist, 'annorect')[0], 'objpos') #r_idx = 0인 사람의 objpos 뽑음
-        posX, posY = getattr(objpos, 'x'), getattr(objpos, 'y')
-        image = crop_image(image, (posX, posY), 256)
 
-        heatmap = np.zeros(shape=(64, 64, self.joint_num), dtype=np.float32)
+        scale = annorect.scale
+        hitbox = 200 * scale
 
-        keypoint = np.zeros(shape=(self.joint_num, 2))
-        
+        objpos = getattr(annorect, 'objpos')
+        center = Vector2(getattr(objpos, 'x'), getattr(objpos, 'y'))
+        ret_image = crop_image(image, center, 256)
 
-    
+        assert ret_image.shape == (256, 256, 3)
+
+        ret_heatmap = np.zeros(shape=(64, 64, self.joint_num), dtype=np.float32)
+
+        ret_keypoint = np.zeros(shape=(self.joint_num, 2))
+
+        keypoints = annorect.annopoints.point
+
+        for key_idx in range(keypoints.shape[0]):
+            joint_id = keypoints[key_idx].id
+            keypoint = Vector2(keypoints[key_idx].x, keypoints[key_idx].y)
+            keypoint -= (center - hitbox / 2)
+
+            if min(keypoint) < 0 or max(keypoint) >= 64:
+                continue
+
+            ret_heatmap[:, :, joint_id] = generate_heatmap(64, keypoint.y, keypoint.x)
+            ret_keypoint[joint_id, :] = [keypoint.y, keypoint.x]
+
+        act = getattr(self.mat['RELEASE'], 'act')[img_idx]
+        ret_activity = act.act_id
+
+        return ret_image, ret_heatmap, ret_keypoint, ret_activity
 
 mpii = MPII(batch_size=100, task='train')
--- util.py
+++ util.py
@@ -1,1 +1,37 @@
+import numpy as np
+from functools import lru_cache
 def crop_image(image, center, res):
+    return np.zeros(shape=[256, 256, 3])
+
+@lru_cache(maxsize=32)
+def gaussian(size, sigma=0.25, mean=0.5):
+    width = size
+    heigth = size
+    amplitude = 1.0
+    sigma_u = sigma
+    sigma_v = sigma
+    mean_u = mean * width + 0.5
+    mean_v = mean * heigth + 0.5
+
+    over_sigma_u = 1.0 / (sigma_u * width)
+    over_sigma_v = 1.0 / (sigma_v * heigth)
+
+    x = np.arange(0, width, 1, np.float32)
+    y = x[:, np.newaxis]
+
+    du = (x + 1 - mean_u) * over_sigma_u
+    dv = (y + 1 - mean_v) * over_sigma_v
+
+    return amplitude * np.exp(-0.5 * (du * du + dv * dv))
+
+
+def generate_heatmap(size, y0, x0, pad=3):
+    y0, x0 = int(y0), int(x0)
+    dst = [max(0, y0 - pad), max(0, min(size, y0 + pad + 1)), max(0, x0 - pad), max(0, min(size, x0 + pad + 1))]
+    src = [-min(0, y0 - pad), pad + min(pad, size - y0 - 1) + 1, -min(0, x0 - pad), pad + min(pad, size - x0 - 1) + 1]
+
+    heatmap = np.zeros([size, size])
+    g = gaussian(7)
+    heatmap[dst[0]:dst[1], dst[2]:dst[3]] = g[src[0]:src[1], src[2]:src[3]]
+
+    return heatmap
