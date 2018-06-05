06aa5c14687e1801acba032f71863aa7d47f3473
tata42 <fldjzksms@gmail.com>
2018-06-05 20:14:16 +0900

    edit loader

--- MK_MPII_loader.py
+++ MK_MPII_loader.py
@@ -25,12 +25,12 @@ class MPII:
 
         for img_idx in range(self.image_num):
             if task == 'train' and getattr(self.mat['RELEASE'], 'img_train')[img_idx] == 1:
-                annorect = getattr(getattr(self.mat['RELEASE'], 'annolist'), 'annorect')
+                annorect = getattr(getattr(self.mat['RELEASE'], 'annolist')[img_idx], 'annorect')
                 if type(annorect) is np.ndarray:
                     for r_idx in range(len(annorect)):
                         self.image_set.append((img_idx, r_idx))
                 else:
-                    self.image_set.append(img_idx, -1)
+                    self.image_set.append((img_idx, -1))
 
     def __iter__(self):
         return self
@@ -57,11 +57,11 @@ class MPII:
         batch_heatmap = np.zeros(shape=(len(batch_image), 64, 64, self.joint_num), dtype=np.float32)
         batch_keypoint = np.zeros(shape=(len(batch_image), self.joint_num, 2), dtype=np.float32)
         batch_activity = np.zeros(shape=(len(batch_image), 1), dtype=np.int32)
-
+        batch_threshold = np.zeros(shape=(len(batch_image), 1), dtype=np.float32)
         for idx in range(len(batch_image)):
-            batch_rgb[idx], batch_heatmap[idx], batch_keypoint[idx], batch_activity[idx] = self.get_data(batch_image[idx])
+            batch_rgb[idx], batch_heatmap[idx], batch_keypoint[idx], batch_activity[idx], batch_threshold[idx] = self.get_data(batch_image[idx])
 
-        return batch_rgb, batch_heatmap, batch_keypoint, batch_activity
+        return batch_rgb, batch_heatmap, batch_keypoint, batch_activity, batch_threshold
 
     def get_data(self, idx):
         img_idx, r_idx = idx
@@ -79,7 +79,8 @@ class MPII:
 
         objpos = getattr(annorect, 'objpos')
         center = Vector2(getattr(objpos, 'x'), getattr(objpos, 'y'))
-        ret_image = crop_image(image, center, 256)
+        # ret_image = crop_image(image, center, 256)
+        ret_image = crop_image(image, center, scale, 0.0, 256)
 
         assert ret_image.shape == (256, 256, 3)
 
@@ -91,18 +92,18 @@ class MPII:
 
         for key_idx in range(keypoints.shape[0]):
             joint_id = keypoints[key_idx].id
-            keypoint = Vector2(keypoints[key_idx].x, keypoints[key_idx].y)
-            keypoint -= (center - hitbox / 2)
+            in_rgb = Vector2(keypoints[key_idx].x, keypoints[key_idx].y) # input RGB coordinate.
+            in_heatmap = (in_rgb - center) * 64 / (200 * scale) + Vector2(32, 32)
+            keypoint = in_heatmap
 
             if min(keypoint) < 0 or max(keypoint) >= 64:
                 continue
 
-            ret_heatmap[:, :, joint_id] = generate_heatmap(64, keypoint.y, keypoint.x)
+            ret_heatmap[:, :, joint_id] = generate_heatmap(64, keypoint.y, keypoint.x) # cropped RGB coordinate.
             ret_keypoint[joint_id, :] = [keypoint.y, keypoint.x]
 
         act = getattr(self.mat['RELEASE'], 'act')[img_idx]
         ret_activity = act.act_id
+        ret_threshold = 25.6
+        return ret_image, ret_heatmap, ret_keypoint, ret_activity, ret_threshold
 
-        return ret_image, ret_heatmap, ret_keypoint, ret_activity
-
-mpii = MPII(batch_size=100, task='train')
